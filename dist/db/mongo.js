70a00f8bc466c667cfeb2024763d5cb8804ca187
AkashbabuNA <akash.babu@nestaway.com>
2018-07-22 22:43:37 +0530

    Initial Commit

--- dist/db/mongo.js
+++ dist/db/mongo.js
@@ -1,8 +1,6 @@
-'use strict';Object.defineProperty(exports, "__esModule", { value: true });exports.GetObjId = undefined;var _mongojs = require('mongojs');var _mongojs2 = _interopRequireDefault(_mongojs);
+'use strict';Object.defineProperty(exports, "__esModule", { value: true });var _mongojs = require('mongojs');var _mongojs2 = _interopRequireDefault(_mongojs);
 var _config = require('../config');var _config2 = _interopRequireDefault(_config);function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}
 
-var db = (0, _mongojs2.default)(_config2.default.db.mongo.url);
-
-var GetObjId = exports.GetObjId = function GetObjId(id) {return _mongojs2.default.ObjectId(id);};exports.default =
+var db = (0, _mongojs2.default)(_config2.default.db.mongo.url);exports.default =
 
 db;
