7e506d4c6837cb768f8befe13ac996908c3d8c09
AkashbabuNA <akash.babu@nestaway.com>
2018-09-06 00:06:24 +0530

    Removed dist folder

--- dist/lib/logger.js
+++
@@ -1,5 +0,0 @@
-'use strict';Object.defineProperty(exports, "__esModule", { value: true });var _interfaceLogger = require('interface-logger');var _interfaceLogger2 = _interopRequireDefault(_interfaceLogger);function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}
-
-var logger = new _interfaceLogger2.default('ms-registry', 'tempPwd');exports.default =
-
-logger;
